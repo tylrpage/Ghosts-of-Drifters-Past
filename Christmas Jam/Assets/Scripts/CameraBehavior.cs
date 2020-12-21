e3531707581ad293bc4e19a9247ced125fba23b1
dj46563 <tylrpage@gmail.com>
2020-12-21 15:10:15 -0800

    Improving car feel

--- Christmas Jam/Assets/Scripts/CameraBehavior.cs
+++ Christmas Jam/Assets/Scripts/CameraBehavior.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {
-    private Transform _parent;
+    public Transform SphereTarget;
+    public Transform ModelTarget;
+
+    public Vector3 Offset;
+
+    private Vector3 IdealPosition => ModelTarget.position + ModelTarget.rotation * Offset;
+
     // Start is called before the first frame update
     void Start()
     {
-        _parent = transform.parent;
+        transform.position = SphereTarget.position + Offset;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.forward = _parent.position - transform.position;
+        transform.position = Vector3.Lerp(transform.position, IdealPosition, 2 * Time.deltaTime);
+        //transform.position = IdealPosition;
+        // transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(SphereTarget.position - transform.position, Vector3.up),
+        //     2 * Time.deltaTime);
+        transform.rotation = Quaternion.LookRotation(ModelTarget.position - transform.position, Vector3.up);
     }
 }
--- Christmas Jam/Assets/Scripts/Car.cs
+++ Christmas Jam/Assets/Scripts/Car.cs
@@ -7,13 +7,16 @@ public class Car : MonoBehaviour
 {
     [SerializeField] private Rigidbody Sphere;
     [SerializeField] private Transform Model;
-    public float ForceMult;
+    public float ForceMultMax;
     public float RotSpeed;
+    public float Drag;
+    public float DriftDrag;
     
-    private readonly Vector3 _initialSphereOffset = new Vector3(0, 0.5f, 0);
+    private readonly Vector3 _initialSphereOffset = new Vector3(0, 2f, 0);
 
     private Vector3 _inputDir;
     private float _speed;
+    private float _forceMult;
     
     // Start is called before the first frame update
     void Start()
@@ -24,16 +27,31 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _inputDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        transform.RotateAround(Sphere.position, Vector3.up, _inputDir.x * RotSpeed * _speed * Time.deltaTime);
+        _inputDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
-        Model.position = Sphere.position - _initialSphereOffset;
-        //Model.forward = Sphere.velocity;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Sphere.drag = DriftDrag;
+            _forceMult = ForceMultMax / 3f;
+            transform.RotateAround(Sphere.position, Vector3.up, _inputDir.x * RotSpeed * Sphere.velocity.magnitude * Time.deltaTime);
+        }
+        else
+        {
+            Sphere.drag = Drag;
+            _forceMult = ForceMultMax;
+            transform.RotateAround(Sphere.position, Vector3.up, _inputDir.x * RotSpeed * _speed * Time.deltaTime);
+        }
     }
 
     private void FixedUpdate()
     {
-        Sphere.AddForce(transform.forward * _inputDir.z * ForceMult, ForceMode.Force);
-        _speed = Sphere.velocity.magnitude;
+        Sphere.AddForce(transform.forward * _inputDir.z * _forceMult, ForceMode.Force);
+        _speed = Vector3.Dot(transform.forward, Sphere.velocity);
+    }
+
+    private void LateUpdate()
+    {
+        Model.position = Vector3.Lerp(Model.position, Sphere.position - _initialSphereOffset, 10f * Time.deltaTime);
+        //Model.position = Sphere.position - _initialSphereOffset;
     }
 }
