cae0c85292794430d2bf7bd72cb18f1badca173f
dj46563 <tylrpage@gmail.com>
2020-12-21 15:38:40 -0800

    Added rotation clamps to car turning

--- Christmas Jam/Assets/Scripts/CameraBehavior.cs
+++ Christmas Jam/Assets/Scripts/CameraBehavior.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour
 {
-    public Transform SphereTarget;
     public Transform ModelTarget;
+    public Rigidbody sphereRigidbody;
 
     public Vector3 Offset;
 
-    private Vector3 IdealPosition => ModelTarget.position + ModelTarget.rotation * Offset;
+    private Vector3 IdealPosition => ModelTarget.position + ModelTarget.rotation * Offset * (1 + 0.2f * sphereRigidbody.velocity.magnitude);
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = SphereTarget.position + Offset;
+        transform.position = ModelTarget.position + Offset;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, IdealPosition, 2 * Time.deltaTime);
+        transform.position = Vector3.Slerp(transform.position - ModelTarget.position,
+            IdealPosition - ModelTarget.position, 2 * Time.deltaTime) + ModelTarget.position;
+        transform.position = Vector3.Lerp(transform.position, IdealPosition, 1 * Time.deltaTime);
         //transform.position = IdealPosition;
         // transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(SphereTarget.position - transform.position, Vector3.up),
         //     2 * Time.deltaTime);
--- Christmas Jam/Assets/Scripts/Car.cs
+++ Christmas Jam/Assets/Scripts/Car.cs
@@ -17,6 +17,7 @@ public class Car : MonoBehaviour
     private Vector3 _inputDir;
     private float _speed;
     private float _forceMult;
+    private float _driftRotMult = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -28,18 +29,22 @@ public class Car : MonoBehaviour
     void Update()
     {
         _inputDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        float rotationMult = RotSpeed * Sphere.velocity.magnitude;
+        Debug.Log(rotationMult);
 
         if (Input.GetKey(KeyCode.Space))
         {
+            _driftRotMult = Mathf.Clamp(_driftRotMult + Time.deltaTime * 0.5f, 1f, 1.5f);
             Sphere.drag = DriftDrag;
             _forceMult = ForceMultMax / 3f;
-            transform.RotateAround(Sphere.position, Vector3.up, _inputDir.x * RotSpeed * Sphere.velocity.magnitude * Time.deltaTime);
+            transform.RotateAround(Sphere.position, Vector3.up, (_inputDir.x * _driftRotMult) * Mathf.Clamp(rotationMult, 0, 70f) * Time.deltaTime);
         }
         else
         {
+            _driftRotMult = 1;
             Sphere.drag = Drag;
             _forceMult = ForceMultMax;
-            transform.RotateAround(Sphere.position, Vector3.up, _inputDir.x * RotSpeed * _speed * Time.deltaTime);
+            transform.RotateAround(Sphere.position, Vector3.up, _inputDir.x * Mathf.Clamp(rotationMult, 0, 70f) * Time.deltaTime);
         }
     }
 
